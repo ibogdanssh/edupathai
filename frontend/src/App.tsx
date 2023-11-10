dee972605c84da21f47ec526a5c8c4522e946ca1
Constantin Bogdan-Teodor <bogdan.constantin0110@gmail.com>
2023-11-10 21:47:45 +0200

    app tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,12 @@
 import "./App.css";
-import NavBar from "../components/navbarChat";
-import { useState } from "react";
-
-
+import NavBar from "../components/navbarChat"
 
 function App() {
-  const [profilePicture, setProfilePicture] = useState("");
-  
   return (
-    <>
-      <NavBar/>
-      {/* <div>
-        <h1 className="text-3xl font-bold underline">Demo H1</h1>
-        <h2>Demo H2</h2>
-      </div> */}
-
-    </>
+    <div>
+      <h1 className="text-3xl font-bold underline">Demo H1</h1>
+      <h2>Demo H2</h2>
+    </div>
   );
 }
 
